c47a79aaafd6289b66522545781ed609142f5660
navh <navarre.hebb@gmail.com>
2025-01-19 20:21:58 -0500

    add test

---
+++ backend/main_test.go
@@ -0,0 +1,48 @@
+// main_test.go
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	pb "buoyboy/proto"
+
+	"google.golang.org/protobuf/proto"
+)
+
+func TestHandleBuoyReading(t *testing.T) {
+	// Create test message
+
+	testBuoy := &pb.BuoyStatus{
+		BuoyId:              42,
+		ReportId:            12345,
+		Timestamp:           time.Now().Unix(),
+		Latitude:            37.805444,
+		Longitude:           -122.890564,
+		DepthMeters:         0,
+		TempCelsius:         8.5,
+		AccelX:              0.03,
+		AccelY:              -0.01,
+		AccelZ:              0.01,
+		BatteryLevelPercent: 85,
+	}
+
+	// Serialize it
+	data, err := proto.Marshal(testBuoy)
+	if err != nil {
+		t.Fatalf("Failed to marshal protobuf: %v", err)
+	}
+
+	// Send it to test server
+	req := httptest.NewRequest(http.MethodPost, "/buoy", bytes.NewReader(data))
+	w := httptest.NewRecorder()
+	handleBuoyReading(w, req)
+
+	// Check we got OK response
+	if w.Result().StatusCode != http.StatusOK {
+		t.Errorf("Expected status OK; got %v", w.Result().Status)
+	}
+}
